202fc6f9b283def48f86cb77077723cf64f134b9
Kamil Preweda <preweda.kamil@o2.pl>
2023-04-03 10:37:25 +0200

    Dodaj pliki projektów.

---
+++ FlopClub/Models/User.cs
@@ -0,0 +1,12 @@
+﻿namespace FlopClub.Models
+{
+    public class User
+    {
+        public int Id { get; set; }
+        public string Username { get; set; } = string.Empty;
+        public byte[] PasswordHash { get; set; } = new byte[0];
+        public byte[] PasswordSalt { get; set; } = new byte[0];
+        public int HandsPlayed { get; set; }
+        public decimal TotalWinnings { get; set; }
+    }
+}
